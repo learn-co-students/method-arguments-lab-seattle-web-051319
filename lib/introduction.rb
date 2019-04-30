3403477a5433a65c9244fb8b5dccce0a03e3f1f2
Michael Maggs <michaelmaggs@gmail.com>
2019-04-30 20:55:25 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with-language(name, language)
+  puts "Hi, my name is #{name} amd I am learning to program in #{language}."
+end
